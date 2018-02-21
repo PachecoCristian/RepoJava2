49e376d44aad507d691250bcb9a41188a2bbba06
PachecoCristian <36259537+PachecoCristian@users.noreply.github.com>
2018-02-21 13:35:39 +0100

    Update Clase1.java
    creardo un metodo que comprueba si es par

--- ProyectoGit2/src/Parte1/Clase1.java
+++ ProyectoGit2/src/Parte1/Clase1.java
@@ -8,7 +8,11 @@ public class Clase1 {
 		
 		Random rng = new Random();
 		
+		
 		System.out.println("El numero de la loteria de hoy es "+(rng.nextInt(50)+1));
 	}
-
+	public static String espar(int num){
+		if(num%2=0 )return "es par";
+		else return "No es par";
+	}
 }
