17f379d1851459e26d99fd38c32b99eb3e83181f
alumno <alumno@PC16>
2018-02-21 13:38:27 +0100

    creamos una variable num para guardar el numero

--- ProyectoGit2/src/Parte1/Clase1.java
+++ ProyectoGit2/src/Parte1/Clase1.java
@@ -7,8 +7,9 @@ public class Clase1 {
 	public static void main(String[] args) {
 		
 		Random rng = new Random();
+		int num = rng.nextInt(50)+1;
 		
-		System.out.println("El numero de la loteria de hoy es "+(rng.nextInt(50)+1));
+		System.out.println("El numero de la loteria de hoy es "+num );
 	}
 
 }
